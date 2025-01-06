ddb65492a4078aadba53ade5bc92a97745198dc3
akchonya <akchonyahokagivna@gmail.com>
2025-01-06 12:23:25 +0200

    🥢 chore: delete unused

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/react-gsheets-demo/',  // Changed back to match your repo name
-  plugins: [react()],
-}) 
